0170a075ea001d590bf00c76773ff1f5093b3e69
Saurabh-yogi <saurabh.24bcan0305@jecrcu.edu.in>
2026-08-02 01:27:49 +0530

    server.js

--- fleet-backened/server.js
+++ fleet-backened/server.js
@@ -10,14 +10,14 @@ const connectDB = require('./CONFIG.JS/db');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Connect Database
 connectDB();
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000'}));
 app.use(helmet());
 
 const limiter = rateLimit({
